ca97dbbbb2a5a12ce6a6f0aa05c786c3bc1084c0
Sarkheel786Javed <sarkheel@kodware.net>
2024-09-14 18:56:57 -0700

    mnbvm

--- BLOCKCHAIN/src/App.jsx
+++ BLOCKCHAIN/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import {BrowserRouter ,Routes , Route} from 'react-router-dom'
 import Layout from './app/Layout/Layout.jsx'
-import HomePage from '../src/Components/HomePage/HomePage.jsx'
+import HomePage from '../src/Components/HomePage/HomePage'
+import About from '../src/Components/AboutPage/About.jsx'
 function App() {
 
   return (
     <BrowserRouter>
      <Routes>
       <Route path="/" element={<Layout><HomePage/></Layout>}></Route>
+      <Route path="/about" element={<Layout><About/></Layout>}></Route>
      </Routes>
     </BrowserRouter>
   )
